28cdf6d0a64be440ff7d67c25da842454fc229ae
Lisa Yi <yeefangxu@gmail.com>
2022-08-10 22:57:01 -0400

    add comments module

---
+++ comments/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from tweets.models import Tweet
+
+
+class Comment(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    tweet = models.ForeignKey(
+        Tweet,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    content = models.TextField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        index_together = (
+            ('tweet','created_at'),
+        )
+
+    def __str__(self):
+        return f"{self.created_at} - {self.user} says {self.content} at tweet {self.tweet_id}"
