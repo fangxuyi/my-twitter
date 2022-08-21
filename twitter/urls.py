4aa6561e42a61752f8f05021524fe68801620405
Lisa Yi <yeefangxu@gmail.com>
2022-08-21 18:24:46 -0400

    add notifications api

--- twitter/urls.py
+++ twitter/urls.py
@@ -21,6 +21,7 @@ from tweets.api.views import TweetViewSet
 from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
 from likes.api.views import LikeViewSet
+from inbox.api.views import NotificationViewSet
 
 import debug_toolbar
 
@@ -32,6 +33,7 @@ router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
 router.register(r'api/likes', LikeViewSet, basename='likes')
+router.register(r'api/notifications', NotificationViewSet, basename='notifications')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
