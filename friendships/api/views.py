f03109c07fbe0323c12a3bc729fbbd44ddaa56c5
Lisa Yi <yeefangxu@gmail.com>
2022-09-01 21:54:42 -0400

    add pagination

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -9,11 +9,13 @@ from friendships.api.serializers import (
     FriendshipSerializerForCreate,
 )
 from django.contrib.auth.models import User
+from utils.paginations import FriendshipPagination
 
 
 class FriendshipViewSet(viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = FriendshipSerializerForCreate
+    pagination_class = FriendshipPagination
 
     def list(self, request):
         #GET /api/friendships/
@@ -23,21 +25,17 @@ class FriendshipViewSet(viewsets.GenericViewSet):
     def followers(self, request, pk):
         #GET /api/friendships/1/followers/
         friendships = Friendship.objects.filter(to_user_id=pk).order_by("-created_at")
-        serializer = FollowerSerializer(friendships, many=True)
-        return Response(
-            {'followers': serializer.data},
-            status=status.HTTP_200_OK,
-        )
+        page = self.paginate_queryset(friendships)
+        serializer = FollowerSerializer(page, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followings(self, request, pk):
         #GET /api/friendships/1/following/
         friendships = Friendship.objects.filter(from_user_id=pk).order_by("-created_at")
-        serializer = FollowingSerializer(friendships, many=True)
-        return Response(
-            {'followings': serializer.data},
-            status=status.HTTP_200_OK,
-        )
+        page = self.paginate_queryset(friendships)
+        serializer = FollowingSerializer(page, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
 
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
     def follow(self, request, pk):
@@ -58,11 +56,10 @@ class FriendshipViewSet(viewsets.GenericViewSet):
              "errors": serializer.errors,
             }, status=status.HTTP_400_BAD_REQUEST)
 
-        serializer.save()
-        return Response({
-                'success': True,
-                'duplicate': False,
-            }, status=status.HTTP_201_CREATED)
+        instance = serializer.save()
+        return Response(
+            FollowingSerializer(instance, context={'request': request}).data,
+            status=status.HTTP_201_CREATED)
 
 
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
